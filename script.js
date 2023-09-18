3b4c8879934f6be3b33533a620a30c2d1e370238
Benjamin <legoawesomepants@gmail.com>
2023-09-18 12:51:12 -0500

    Re-factored the code for much better project organization

--- script.js
+++ script.js
@@ -1,60 +1,55 @@
+var gameplayScene = new Phaser.Class({
+  Extends: Phaser.scene,
+  init: function(){},
+  preload: function(){},
+  modal: function(modalText, miliseconds, andThen){
+    const modalBackgrounds = this.add.graphics();
+    modalBackgrounds.fillStyle(0x2d2d2d, 0.5);
+    const modalTextObject = this.add.text(
+      0,
+      (config.height / 2) - 15,
+      modalText,
+      {fontSize:"30px",color:"white",fontFamily:"Trebuchet MS"}
+    );
+    modalTextObject.x = (config.width/2) - (modalTextObject.width/2);
+    this.tweens.addCounter({
+
+      from: 0,
+      to: 1,
+      duration: 500,
+      onUpdate: (tween) => {
+        let t = tween.getValue();
+        modalBackgrounds.clear();
+        
+        
+        modalBackgrounds.fillRect(((config.width/2) - (modalTextObject.width/2) - 30), ((config.height/2)-(30 * t)), (modalTextObject.width + 60), 60*t);
+      }
+    })
     
-const words = ["mississippi","nor", "double", "seat", "arrive", "master", "track", "parent", "shore", "division", "sheet", "substance", "favor", "connect", "post", "spend", "chord", "fat", "glad", "original", "share", "station", "dad", "bread", "charge", "proper", "bar", "offer", "segment", "slave", "duck", "instant", "market", "degree", "populate", "chick", "dear", "enemy", "reply", "drink", "occur", "support", "speech", "nature", "range", "steam", "motion", "path", "liquid", "log", "meant", "quotient", "teeth", "shell", "neck", "oxygen", "sugar", "death", "pretty", "skill", "women", "season", "solution", "magnet", "silver", "thank", "branch", "match", "suffix", "especially", "fig", "afraid", "huge", "sister", "steel", "discuss", "forward", "similar", "guide", "experience", "score", "apple", "bought", "led", "pitch", "coat", "mass", "card", "band", "rope", "slip", "win", "dream", "evening", "condition", "feed", "tool", "total", "basic", "smell", "valley"];
-const targetWPM = 9;
-      var config = {
-        type: Phaser.AUTO,
-        width: 1080,
-        height: 400,
-        backgroundColor: "#003399",
-        scene: {
-          preload: preload,
-          create: create,
-          update: update
-        },
-      };
-
-      function preload() {}
-
-      function create() {
+    this.timedEvent = this.time.delayedCall(
+      miliseconds,
+      () => {
+        modalTextObject.destroy();
+        modalBackgrounds.clear();
+        if(andThen){
+          andThen();
+        }
+        
+      },
+      [],
+      this
+    );
+  },
+  finishedEmitter:new Phaser.Events.EventEmitter(),
+  create: function() {
+    //put these somewhere else?
+    const words = ["mississippi","nor", "double", "seat", "arrive", "master", "track", "parent", "shore", "division", "sheet", "substance", "favor", "connect", "post", "spend", "chord", "fat", "glad", "original", "share", "station", "dad", "bread", "charge", "proper", "bar", "offer", "segment", "slave", "duck", "instant", "market", "degree", "populate", "chick", "dear", "enemy", "reply", "drink", "occur", "support", "speech", "nature", "range", "steam", "motion", "path", "liquid", "log", "meant", "quotient", "teeth", "shell", "neck", "oxygen", "sugar", "death", "pretty", "skill", "women", "season", "solution", "magnet", "silver", "thank", "branch", "match", "suffix", "especially", "fig", "afraid", "huge", "sister", "steel", "discuss", "forward", "similar", "guide", "experience", "score", "apple", "bought", "led", "pitch", "coat", "mass", "card", "band", "rope", "slip", "win", "dream", "evening", "condition", "feed", "tool", "total", "basic", "smell", "valley"];
+    const targetWPM = 9;
+          
         const mBackgrounds = this.add.graphics();
         mBackgrounds.fillStyle(0x2d2d2d, 0.5);
-        const finishedEmitter = new Phaser.Events.EventEmitter();
-        const modal = (modalText, miliseconds, andThen) => {
-          const modalTextObject = this.add.text(
-            0,
-            (config.height / 2) - 15,
-            modalText,
-            {fontSize:"30px",color:"white",fontFamily:"Trebuchet MS"}
-          );
-          modalTextObject.x = (config.width/2) - (modalTextObject.width/2);
-          this.tweens.addCounter({
-
-            from: 0,
-            to: 1,
-            duration: 500,
-            onUpdate: (tween) => {
-              let t = tween.getValue();
-              mBackgrounds.clear();
-              
-              
-              mBackgrounds.fillRect(((config.width/2) - (modalTextObject.width/2) - 30), ((config.height/2)-(30 * t)), (modalTextObject.width + 60), 60*t);
-            }
-          })
-          
-          this.timedEvent = this.time.delayedCall(
-            miliseconds,
-            () => {
-              modalTextObject.destroy();
-              mBackgrounds.clear();
-              if(andThen){
-                andThen();
-              }
-              
-            },
-            [],
-            this
-          );
-        };
+       // const finishedEmitter = 
+        
         const marquee = (
           typingText,
           originX,
@@ -113,7 +108,25 @@ const targetWPM = 9;
             fontSize: fontsize.toString() + "px",
           });
           invisibleLetter.visible = false;
+          const finishingHandler = () => {
+            /* Handling what happens when we finish typing the marquee*/
 
+            finishedTime = new Date();
+            mBackgrounds.clear();
+            //rect.destroy();
+            console.log("time to finish: " + finishedTime - spawningTime);
+            const finalWPM =
+              amountOfWords / ((finishedTime - spawningTime) / 60000);
+            console.log("words per minute: " + finalWPM);
+            const accuracy = accuratekeysPressed/keysPressed;
+            invisibleLetter.destroy();
+            typingTextDisplay.destroy();
+            this.currentTextObject = false;
+            this.input.keyboard.removeListener("keydown", keyPressHandler);
+            //emit an event to signify that the user has succeeded in typing the string
+            this.finishedEmitter.emit("Finished", finalWPM, accuracy);
+          }
+          this.finishedEmitter.on("loss",finishingHandler);
           const keyPressHandler = (keyPress) => {
             if(!spawningTime){
                 spawningTime = new Date();
@@ -134,27 +147,12 @@ const targetWPM = 9;
                 typingTextDisplay.x -= fontsize*5;
               }
               if (typingTextDisplay.text.length === 1) {
-                /* Handling what happens when we finish typing the marquee*/
-
-                finishedTime = new Date();
-                mBackgrounds.clear();
-                //rect.destroy();
-                console.log("time to finish: " + finishedTime - spawningTime);
-                const finalWPM =
-                  amountOfWords / ((finishedTime - spawningTime) / 60000);
-                console.log("words per minute: " + finalWPM);
-                const accuracy = accuratekeysPressed/keysPressed;
-                invisibleLetter.destroy();
-                typingTextDisplay.destroy();
-                this.currentTextObject = false;
-                this.input.keyboard.removeListener("keydown", keyPressHandler);
-                //emit an event to signify that the user has succeeded in typing the string
-                finishedEmitter.emit("Finished", finalWPM, accuracy);
+                finishingHandler();
               }
             } else if (keyPress.key !== "Shift") {
               keysPressed++;
               //emit an event to signify that the user has made a mistake
-              finishedEmitter.emit("Mistake");
+              this.finishedEmitter.emit("Mistake");
             }
           };
           this.input.keyboard.on("keydown", keyPressHandler);
@@ -216,13 +214,16 @@ const targetWPM = 9;
             Phaser.Math.RND.integerInRange(30, config.height - 30),
             { wpm: incrementalWPM }
           );
-          finishedEmitter.on("Mistake", ()=>{
-            console.log("finishedEmitter activated")
-            this.currentTextObject.x -= 30;
+          this.finishedEmitter.on("Mistake", ()=>{
+            //console.log("finishedEmitter activated")
+            this.currentTextObject.x -= 15;
           })
-          finishedEmitter.on("Finished", (WPM, accuracy) => {
-            modal("WPM: " + Math.floor(WPM) + " Accuracy: " + Math.floor(accuracy*100) + "%", 3000, again);
-          });
+          this.finishedEmitter.on("Finished", (WPM, accuracy) => {
+            if(!this.gameEnded){
+              this.modal("WPM: " + Math.floor(WPM) + " Accuracy: " + Math.floor(accuracy*100) + "%", 3000, again);
+          
+            }
+            });
         };
         const startingText = this.add.text(
           0,
@@ -233,23 +234,45 @@ const targetWPM = 9;
         const startGame = () => {
           console.log("startGame");
           startingText.destroy();
-          modal("Get ready!",3000,()=>{
+          this.modal("Get ready!",3000,()=>{
             infiniteMode(60);
           })
           this.input.keyboard.removeListener("keydown", startGame);
         }
         this.input.keyboard.on('keydown',startGame)
 
-      }
-      function update(time, delta) {
+      },
+      gameEnded:false,
+      update: function(time, delta) {
+        
         let f = 1000 / 60 / delta;
         if (this.currentTextObject) {
           this.currentTextObject.x -= f * this.currentTextObject.speed;
-          if (this.currentTextObject.x < -100) {
-            alert("wow you lost sucker");
-            this.sys.game.destroy(true);
+          if (this.currentTextObject.x < -100 && !this.gameEnded) {
+            this.gameEnded = true;
+            this.finishedEmitter.emit("loss")
+            //mBackgrounds.clear();
+            console.log("running this code again...")
+            this.modal("Wowie wowie good job Joe Schmoe",10000)
           }
         }
       }
+
+})   
+
+
+
+var config = {
+        type: Phaser.AUTO,
+        width: 1080,
+        height: 400,
+        backgroundColor: "#003399",
+        scene: [gameplayScene],
+      };
+
+      //function preload() {}
+
+      
+  
 
       var game = new Phaser.Game(config);
